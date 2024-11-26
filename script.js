6162cec3e30ef3b88606f8c449796c240fbd18c4
vasanthfeb <vasanthfeb18@gmail.com>
2024-11-26 07:31:55 +0530

    sticky master

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ function form_fun(){
     alert(name_name +" ! "+"Your form has been Successfully Submitted")
     
 }
+
+
+// var seconds = 10; //**change 120 for any number you want, it's the seconds **//
+// function secondPassed() {
+//     var minutes = Math.round((seconds - 30)/60);
+//     var remainingSeconds = seconds % 60;
+//     if (remainingSeconds < 10) {
+//         remainingSeconds = "0" + remainingSeconds;  
+//     }
+//     document.getElementById('countdown').innerHTML = minutes + ":" + remainingSeconds;
+//     if (seconds == 0) {
+//         clearInterval(countdownTimer);
+//         document.getElementById('countdown').innerHTML = "Last chance!";
+//     } else {
+//         seconds--;
+//     }
+// }
+ 
+// var countdownTimer = setInterval('secondPassed()', 1000);
